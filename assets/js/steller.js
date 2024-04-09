ef5c927c1e34648572b5585d9d96601abc5939c4
ronan000 <64355509+ronan000@users.noreply.github.com>
2024-04-10 01:25:24 +0800

    Update steller.js

--- assets/js/steller.js
+++ assets/js/steller.js
@@ -36,4 +36,42 @@ $(document).ready(function(){
             });
         } 
     });
+
+    // Image zoom
+    $(".zoom-button").on('click', function(event) {
+        event.preventDefault();
+        // Get the source of the image associated with the clicked zoom button
+        var imgSrc = $(this).closest('.img-wrapper').find('img').attr('src');
+        // Create modal markup
+        var modalMarkup = `
+            <div class="modal fade" id="imageModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-lg">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="btn-close" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <img src="${imgSrc}" class="img-fluid modal-img" alt="Zoomed Image">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+
+	    var description = $(this).closest('.img-wrapper').find('h5').text();
+	    // Update the modal body with the new image source and description
+	    $('#imageModal .modal-body img').attr('src', imgSrc);
+	    $('#imageModal .modal-body .image-description').text(description);
+        // Remove any existing modals
+        $('.modal').remove();
+        // Append modal markup to body
+        $('body').append(modalMarkup);
+        // Show modal
+        $('#imageModal').modal('show');
+        // Close modal when close button is clicked
+        $('.modal-header .btn-close').on('click', function() {
+            $('#imageModal').modal('hide');
+        });
+    });
+
 });
