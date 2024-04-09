6418e6b40235300991da89207491bc5256dd2af0
ronan000 <64355509+ronan000@users.noreply.github.com>
2024-04-09 14:25:59 +0800

    Update steller.js

--- assets/js/steller.js
+++ assets/js/steller.js
@@ -12,21 +12,28 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
+// Disable right-click
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+    // Show the warning pane
+    $('#rightClickWarning').fadeIn(200);
+    // Hide the warning pane after 3 seconds
+    setTimeout(function(){
+        $('#rightClickWarning').fadeOut(200);
+    }, 3000);
+});
+
 // smooth scroll
 $(document).ready(function(){
-	$(".nav-link").on('click', function(event) {
-
-    	if (this.hash !== "") {
-
-			event.preventDefault();
-
-			var hash = this.hash;
-
-			$('html, body').animate({
-				scrollTop: $(hash).offset().top
-			}, 700, function(){
-				window.location.hash = hash;
-			});
-      	} 
+    $(".nav-link").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
     });
 });
